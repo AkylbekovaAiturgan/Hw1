56a81ef8730e1b71335d0b74a0e970e388d7d0db
Student <zrn@gmail.com>
2026-09-24 16:00:13 +0600

    fourth commit

---
+++ src/TenNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TenNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = n/10;
+        System.out.println(k);
+
+    }
+}
+
