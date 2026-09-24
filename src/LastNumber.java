e37e7c689b7462d1f2d75acd850c6caefb619ce5
Student <zrn@gmail.com>
2026-09-24 15:52:26 +0600

    fourth commit

---
+++ src/LastNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LastNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = a%10;
+        System.out.println(b);
+}
